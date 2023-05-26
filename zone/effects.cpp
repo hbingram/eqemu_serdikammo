f9c98aa4b411392c48f75845d9bfd30b8b5e876f
hbingram <44652150+hbingram@users.noreply.github.com>
2023-05-26 06:14:25 -0500

    Update effects.cpp

--- zone/effects.cpp
+++ zone/effects.cpp
@@ -844,13 +844,13 @@ bool Client::UseDiscipline(uint32 spell_id, uint32 target) {
 	//can we use the spell?
 	const SPDat_Spell_Struct &spell = spells[spell_id];
 	
-	// BRYANT052223-START-: allow any class to use this discipline
+	// BRYANT052223-START-: allow any class to use any discipline
 	uint8 level_to_use = 255;
 	for (int i = 0; i < sizeof(spell.classes); i++)
 	{
 		if (spell.classes[i] < level_to_use) { level_to_use = spell.classes[i]; }
 	}
-	// BRYANT052223-END-: allow any class to use this discipline
+	// BRYANT052223-END-: allow any class to use any discipline
 	
 	if(level_to_use == 255) {
 		Message(Chat::Red, "Your class cannot learn from this tome.");
